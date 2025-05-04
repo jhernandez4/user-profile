4bcef6573318d04cb0ccab462fa2f56b23ffcde5
jhernandez4 <jokhernandez120744@gmail.com>
2025-05-04 14:36:28 -0700

    fix(database.py): add value validation to database table

--- database.py
+++ database.py
@@ -10,15 +10,15 @@ load_dotenv()
 
 class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    username: str = Field(unique=True, index=True)
-    password: str 
-    email: EmailStr = Field(unique=True)
-    first_name: str 
-    last_name: str 
+    username: str = Field(unique=True, index=True, min_length=1, max_length=30)
+    password: str= Field(min_length=1)
+    email: EmailStr = Field(unique=True, max_length=255)
+    first_name: str = Field(min_length=1)
+    last_name: str = Field(min_length=1)
     birthday: datetime 
     biography: str 
     favorite_number: int 
-    profile_picture: str
+    profile_picture: str = Field(default="https://i.imgur.com/vIaC7Uq.png")
 
     created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
