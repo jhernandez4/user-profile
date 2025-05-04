a4f745e6bc9ea26ce5ae07e35d77c283ccbff1d7
jhernandez4 <jokhernandez120744@gmail.com>
2025-05-04 15:52:16 -0700

    feat(main.py): serve static files for images

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from .database import create_db_and_tables
 from .routers import users
+import os
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.mount("/images", StaticFiles(directory="images"), name="images")
+
 
 @app.on_event("startup")
 def on_startup():
