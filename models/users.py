7bcfb7fa284b55bcc98895665a04fd84bb1834eb
jhernandez4 <jokhernandez120744@gmail.com>
2025-05-04 14:35:17 -0700

    refactor: move request and response models to separate folder

---
+++ models/users.py
@@ -0,0 +1,31 @@
+from sqlmodel import Field, SQLModel, create_engine, select
+from pydantic import EmailStr 
+from datetime import datetime, timezone
+
+class UserBase(SQLModel):
+    username: str = Field(unique=True, index=True, min_length=1, max_length=30)
+    email: EmailStr = Field(unique=True, max_length=255)
+    first_name: str = Field(min_length=1)
+    last_name: str = Field(min_length=1)
+    birthday: datetime 
+    biography: str = Field(max_length=255)
+    favorite_number: int
+
+class UserPublic(UserBase):
+    id: int
+    created_at: datetime
+    profile_picture: str
+
+class UserCreate(UserBase):
+    password: str= Field(min_length=1)
+
+class UserUpdate(UserBase):
+    username: str | None = None
+    password: str | None = None
+    email: EmailStr | None = None
+    first_name: str  | None = None
+    last_name: str  | None = None
+    birthday: datetime | None = None
+    biography: str | None = None
+    favorite_number: int | None = None
+    profile_picture: str | None = None
